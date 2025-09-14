f002367b1a720ccb99a6fcc9c0d3e5daed9c1f24
Kevin Sandoval Gutierrez <kevinsand@MacBook-Air-de-Kevin.local>
2025-09-14 03:30:09 -0600

    agregar rutas de usuario a index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import postRoutes from './routes/post.routes.js'
-import userRoutes from './routes/post.routes.js'
+import userRoutes from './routes/user.routes.js'
 import { connectDB } from './db/pg.config.js'
 
 const app = express()
